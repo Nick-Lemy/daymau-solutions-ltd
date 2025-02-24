b71cc9d5c3257bb4eab967740bb1ea47ea35869f
Nick-Lemy <n.kayiranga@alustudent.com>
2025-02-24 22:13:29 +0200

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
     return (
         <header className="-z-40">
-            <div className="md:flex md:justify-between md:px-7 bg-blue-900 text-sm flex justify-center text-white p-3">
+            <div className="md:flex md:justify-between md:px-7 bg-generalblue    text-sm flex justify-center text-white p-3">
                 <p className="hidden md:flex">sales@cadgulf.com <span className="px-2">+971-585808937</span></p>
                 <p>Follow Us</p>
             </div>
@@ -12,16 +12,16 @@ export default function Header() {
                 <img className="w-10" src="/menu.svg" alt="" />
             </div>
             <div className="flex flex-row items-center py-10 min-h-screen bg-no-repeat bg-[url('https://cadgulf.com/wp-content/uploads/2021/06/website-banner-1.jpg?id=16289')] ">
-                <div className="flex flex-col gap-10 w-full justify-center items-center bg-cover h-full w-h-full ">
+                <div className="lg:max-w-6xl lg:mx-auto flex flex-col gap-10 w-full justify-center items-center bg-cover h-full w-h-full ">
                     <h1 className="text-6xl w-fit h-fit text-center text-white font-semibold">
                         Beyond our expertise & experience
                         what sets us apart is our people
                     </h1>
                     <div className="self-start md:flex-row md:justify-evenly w-full flex flex-col">
-                        <a href="" className="text-white font-semibold rounded-3xl py-3 w-fit px-10 bg-blue-700">
+                        <a href="" className="text-white font-semibold hover:bg-hoverblue duration-500 rounded-3xl py-3 w-fit px-10 bg-myblue">
                             Request a Quote
                         </a>
-                        <a href="" className="text-white font-semibold rounded-3xl w-fit py-3 px-10 bg-orange-400">
+                        <a href="" className="text-white font-semibold rounded-3xl hover:bg-hoverblue duration-500 w-fit py-3 px-10 bg-myorange">
                             Free IT Consultation
                         </a>
                     </div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,13 @@
 import React from "react"
 
-function Card() {
+function Card(props: { src: string, title: string, content: string, readmore: string }) {
     return (
-        <div className="flex flex-col p-10 shadow-xl bg-white text-blue-900 rounded-lg shadow-black/50 shat w-fit">
-            <img className="w-20" src="/computer.svg" alt="" />
-            <h3 className="text-lg font-semibold">IT Infrastructure Solutions</h3>
-            <p className="text-gray-500 text-wrap">
-                Matured Approach to Design and <br /> Implementation.
-            </p>
-            <a href="" className="text-blue-900">Read More</a>
-        </div>
+        <a className="flex max-w-xs gap-3 hover:bg-myorange hover:*:text-white cursor-pointer duration-300 flex-col p-10 shadow-xl bg-white text-blue-900 rounded-lg shadow-black/50 shat w-fit">
+            <img className="w-20" src={props.src} alt="" />
+            <h3 className="text-lg font-semibold">{props.title}</h3>
+            <p className="text-gray-500 text-wrap">{props.content}</p>
+            <a href={props.readmore} className="text-blue-900">Read More</a>
+        </a>
     )
 }
 
@@ -17,11 +15,12 @@ export default function Main() {
     return (
         <div className="relative h-svh">
 
-            <div className="absolute md:-top-[35%] -top-[20%] z-40 px-7 py-10 flex flex-wrap gap-5 justify-center">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+            <div className="absolute md:-top-[35%] lg:-top-[20%] -top-[20%] z-40 px-7 py-10 flex flex-wrap gap-5 justify-center w-full">
+                <Card src="https://cadgulf.com/wp-content/uploads/2022/01/IT-Infrastructure-Solutions_icon.png" title="IT Infrastructure Solutions" content="  Matured Approach to Design and <br /> Implementation." readmore="" />
+                <Card src="https://cadgulf.com/wp-content/uploads/2022/01/cad-engineering_icon.png" title="CAD &
+Engineering" content="Address your business through comprehensive Autodesk services." readmore="" />
+                <Card src="https://cadgulf.com/wp-content/uploads/2022/01/networking_icon.png" title="Networking & IT Security" content="Simplified Security Without Impacting Network Performance." readmore="" />
+                <Card src="https://cadgulf.com/wp-content/uploads/2022/01/Structured-cabling_icon.png" title="Structured Cabling For Networks" content="Comprehensive Range of Structured Cabling & Installation Solutions." readmore="" />
             </div>
         </div>
     )
