36c45e556f38c26c131b4b82677707c833da485e
Nick-Lemy <n.kayiranga@alustudent.com>
2025-02-24 19:32:06 +0200

    Header and cards done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
     return (
-        <header>
+        <header className="-z-40">
             <div className="md:flex md:justify-between md:px-7 bg-blue-900 text-sm flex justify-center text-white p-3">
                 <p className="hidden md:flex">sales@cadgulf.com <span className="px-2">+971-585808937</span></p>
                 <p>Follow Us</p>
@@ -11,7 +11,7 @@ export default function Header() {
                 <img className="w-28" src="/logo.jpeg" alt="Logo" />
                 <img className="w-10" src="/menu.svg" alt="" />
             </div>
-            <div className="flex flex-row items-center py-10 min-h-svw bg-no-repeat bg-[url('https://cadgulf.com/wp-content/uploads/2021/06/website-banner-1.jpg?id=16289')] ">
+            <div className="flex flex-row items-center py-10 min-h-screen bg-no-repeat bg-[url('https://cadgulf.com/wp-content/uploads/2021/06/website-banner-1.jpg?id=16289')] ">
                 <div className="flex flex-col gap-10 w-full justify-center items-center bg-cover h-full w-h-full ">
                     <h1 className="text-6xl w-fit h-fit text-center text-white font-semibold">
                         Beyond our expertise & experience
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function Card() {
     return (
-        <div className="flex flex-col p-10 shadow-xl  text-blue-900 rounded-lg shadow-black/50 shat w-fit">
+        <div className="flex flex-col p-10 shadow-xl bg-white text-blue-900 rounded-lg shadow-black/50 shat w-fit">
             <img className="w-20" src="/computer.svg" alt="" />
             <h3 className="text-lg font-semibold">IT Infrastructure Solutions</h3>
             <p className="text-gray-500 text-wrap">
@@ -15,11 +15,14 @@ function Card() {
 
 export default function Main() {
     return (
-        <div className="px-7 py-10 flex flex-wrap gap-5 justify-center">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+        <div className="relative h-svh">
+
+            <div className="absolute md:-top-[35%] -top-[20%] z-40 px-7 py-10 flex flex-wrap gap-5 justify-center">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     )
 }
